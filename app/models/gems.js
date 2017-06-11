ada7cc6cbd23d5da99850b29bfa3fa3e5d932216
Darryl <trigramthree@gmail.com>
2017-06-11 15:53:04 -0400

    Define Gem Model

---
+++ app/models/gems.js
@@ -0,0 +1,13 @@
+// app/models/app.js
+
+// load mongoose since we need it to define a model
+var mongoose = require('mongoose');
+
+module.exports = mongoose.model('Gem', {
+    name : String,
+    price: Number,
+    description: String,
+    images: Array,
+    reviews: Array,
+    done : Boolean
+});
