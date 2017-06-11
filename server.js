c0def390c264b9c145a564fa0be5253c77f519a2
Darryl <trigramthree@gmail.com>
2017-06-11 15:14:08 -0400

    add config/database.js file

--- server.js
+++ server.js
@@ -12,7 +12,7 @@
     var port    = process.env.PORT || 8888;
 
     // configuration ===============================================================
-    //mongoose.connect(database.url);     // connect to mongoDB database on modulus.io
+    mongoose.connect(database.url);     // connect to mongoDB database on modulus.io
 
     app.use(express.static(__dirname + '/public'));                 // set the static files location /public/img will be /img for users
     app.use(morgan('dev'));                                         // log every request to the console
