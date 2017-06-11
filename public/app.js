91646df9bc7bd05135f8136fdee5288f721c88fc
Darryl <trigramthree@gmail.com>
2017-06-11 18:33:58 -0400

    add StoreController and Update index.html

--- public/app.js
+++ public/app.js
@@ -5,10 +5,28 @@ var app = angular.module('store',[]);
 
 app.controller('StoreController', function(){
 
+    this.product = gem;
 
 
 });
 
+var gem = {
+	"name": "Dodecahedron",
+	"price": 2.95,
+	"description": "This is the shape of the universe... shh"
 
 
+	/*"images": [{
+
+			"full": "2.jpg",
+			"thumb": "IMG_31121.JPG"
+
+		},
+		{
+			"full": "IMG_030811.JPG",
+			"thumb": "IMG_030811.JPG"
+		}
+
+*/
+}
 }());
