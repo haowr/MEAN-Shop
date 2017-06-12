9f61f11550ea6c41b45f5aeec7bf947a6a466d48
Darryl <trigramthree@gmail.com>
2017-06-12 12:57:10 -0400

    $valid form validations and ReviewController

--- public/app.js
+++ public/app.js
@@ -25,6 +25,18 @@ app.controller('PanelController', function(){
 
 });
 
+app.controller("ReviewController",function(){
+
+	this.review ={};
+
+	this.addReview= function(product){
+
+		product.reviews.push(this.review);
+		this.review={};
+	};
+
+});
+
 var gems = [{
 	"name": "Dodecahedron",
 	"price": 2.95,
