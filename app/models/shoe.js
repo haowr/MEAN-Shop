5b37ddb0d4c2244c5d94b0012c4465120db30ef3
Darryl <trigramthree@gmail.com>
2017-06-30 19:03:30 -0400

    add-page-property-to-shoe-schema

--- app/models/shoe.js
+++ app/models/shoe.js
@@ -1,13 +1,18 @@
 var mongoose = require('mongoose');
 
 
+//var shoePages = new Schema({
 
+
+//})
 
 module.exports = mongoose.model('Shoe',{
 
         name:String,
         price:Number,
         shoecolor:Array,
+        dago:String,
+        page:Array,
         colors:Array,
         stars:Number,
         hearts:Number,
