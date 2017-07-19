dd282d1c5f658107640baa8eb263fe1d71051dcb
Darryl <trigramthree@gmail.com>
2017-07-19 13:50:50 -0400

    remove-heroku-routes-and-mlab-cloud-database

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 // config/database.js
 
 module.exports = {
-    //url : 'mongodb://localhost/shop'
-      url : 'mongodb://<ohrha>:<tchuva2ed>@ds113063.mlab.com:13063/meanshop'
+    url : 'mongodb://localhost/shop'
+    //url : 'mongodb://<ohrha>:<tchuva2ed>@ds113063.mlab.com:13063/meanshop'
 };
