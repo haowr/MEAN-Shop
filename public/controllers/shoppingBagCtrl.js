a2511277e9a45fb8744953fd77e3843683e8a6ce
Darryl <trigramthree@gmail.com>
2017-09-05 18:59:10 -0400

    WORK-ON-SHOPPINGBAG-LOGIC

--- public/controllers/shoppingBagCtrl.js
+++ public/controllers/shoppingBagCtrl.js
@@ -652,6 +652,17 @@ app.controller('shoppingBagCtrl', function($scope,Shop,$window,$timeout,Auth,Use
                                             //if($scope.grandTotal == $scope.shippingChoice){
                                                // $scope.grandTotal = false;
                                             //}
+                                                                                       if($scope.shoppingBagShoes.length <2){
+                                               $scope.oldtotal = false;
+                                               $scope.grandTotal = false;
+                                               $scope.total = false;
+                                               
+                                                $scope.shoppingCartEmpty = true;
+                                                $scope.totalWithShipping = false;
+                                                 $scope.couponCodeAdded = false;
+                                                $scope.grandTotal = false;
+                                                $scope.beginCheckout = false;
+                                           }
                                             if($scope.oldtotal == 0 ){
                                                 $scope.oldtotal = false;
                                                 $scope.total = false;
@@ -677,6 +688,12 @@ app.controller('shoppingBagCtrl', function($scope,Shop,$window,$timeout,Auth,Use
                                                $scope.oldtotal = false;
                                                $scope.grandTotal = false;
                                                $scope.total = false;
+                                               
+                                                $scope.shoppingCartEmpty = true;
+                                                $scope.totalWithShipping = false;
+                                                 $scope.couponCodeAdded = false;
+                                                $scope.grandTotal = false;
+                                                $scope.beginCheckout = false;
                                            }
                                              
                                             if($scope.grandTotal == $scope.shippingChoice){
@@ -703,7 +720,18 @@ app.controller('shoppingBagCtrl', function($scope,Shop,$window,$timeout,Auth,Use
             
                                              console.log($scope.shoppingBagShoes);
                                              $window.localStorage.setItem('checkoutArray',JSON.stringify($scope.shoppingBagShoes));
-                                             $rootScope.cartItems = $scope.shoppingBagShoes.length;                                          
+                                             $rootScope.cartItems = $scope.shoppingBagShoes.length;    
+                                                                                        if($scope.shoppingBagShoes.length <2){
+                                               $scope.oldtotal = false;
+                                               $scope.grandTotal = false;
+                                               $scope.total = false;
+                                               
+                                                $scope.shoppingCartEmpty = true;
+                                                $scope.totalWithShipping = false;
+                                                 $scope.couponCodeAdded = false;
+                                                $scope.grandTotal = false;
+                                                $scope.beginCheckout = false;
+                                           }                                      
                                             if($scope.oldtotal == 0 ){
                                                 $scope.oldtotal = false;
                                                 $scope.total = false;
