e7a337bc1d61a151f45ceffe8be6499a0e0899b9
haowr <66386798+haowr@users.noreply.github.com>
2021-02-12 14:03:51 -0800

    Update database.js

--- config/database.js
+++ config/database.js
@@ -3,5 +3,5 @@
 module.exports = {
     //url : 'mongodb://localhost/shop'
     //url : 'mongodb://<ohrha>:<tchuva2ed>@ds113063.mlab.com:13063/hoj'
-    url : 'mongodb://logo:logo@ds161913.mlab.com:61913/hoj'
+    url: 'mongodb+srv://ohrha:<tchuva2ed>@hoj.b1zco.mongodb.net/hoj?retryWrites=true&w=majority
 };
